2564e1409194cf1594d7eceb2e64e642aa6208a7
Wu-Can <112384963+Wu-Can@users.noreply.github.com>
2022-12-22 15:13:06 +0800

    Update psi_eps_fun.m

--- subfun/psi_eps_fun.m
+++ subfun/psi_eps_fun.m
@@ -1,7 +1,7 @@
 function psi_eps = psi_eps_fun(s,epsilon,flag)
 %% If flag = 0, calculate the function value of psi;
 %% if flag = 1, calculate the first-order derivative of psi;
-%% if flag = 2, calculate the second-oeder derivative of psi;
+%% if flag = 2, calculate the second-order derivative of psi;
 
 if flag == 0
     psi_eps = (sqrt(4*epsilon^2 + s.^2) + s)/2; 
