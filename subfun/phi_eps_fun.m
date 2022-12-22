c8409763fda54a46422e84b0503f3b12783e92f8
Wu-Can <112384963+Wu-Can@users.noreply.github.com>
2022-12-22 15:12:47 +0800

    Update phi_eps_fun.m

--- subfun/phi_eps_fun.m
+++ subfun/phi_eps_fun.m
@@ -1,7 +1,7 @@
 function phi_eps = phi_eps_fun(t,epsilon,flag)
 %% If flag = 0, calculate the function value of phi;
 %% if flag = 1, calculate the first-order derivative of phi;
-%% if flag = 2, calculate the second-oeder derivative of phi;
+%% if flag = 2, calculate the second-order derivative of phi;
 
 if flag == 0
     phi_eps = sqrt(4*epsilon^2 + t.^2);
