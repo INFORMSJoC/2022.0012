c47b4dee9c44928644b07b95015ac7eba5c60c97
Wu-Can <112384963+Wu-Can@users.noreply.github.com>
2022-12-23 14:09:54 +0800

    Delete subfun directory

--- subfun/Matvecnx.m
+++
@@ -1,8 +0,0 @@
-%% compute matrix*vector: (I_n+A^T*A)*x
-% function Mx = Matvecnx(ATAmap,B,x)
-% Mx = B^2*x + feval(ATAmap,x);
-% end
-
-function Mx = Matvecnx(ATAmap,x)
-Mx = x + feval(ATAmap,x);
-end
